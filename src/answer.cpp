e03b62903998184878e8b040b46c1b0b693e66df
zhsyourai <zhsyourai@163.com>
2017-08-29 20:02:48 +0800

    Fix bugs

--- src/answer.cpp
+++ src/answer.cpp
@@ -15,3 +15,24 @@ order_range::order_range(int32_t x, int32_t y, int32_t width, int32_t height, in
 answer::answer():_type(answer_type::ANSWER_NONE) {}
 
 answer::answer(answer_type _type) : _type(_type) {}
+void answer::set_type() {
+
+}
+void answer::set_code() {
+
+}
+void answer::set_ranges() {
+
+}
+answer_type answer::get_type() {
+  return answer_type::ANSWER_RANGE;
+}
+std::string answer::get_code() {
+  return std::__cxx11::string();
+}
+std::vector<order_range> answer::get_ranges() {
+  return std::vector<order_range>();
+}
+void answer::clear() {
+
+}
